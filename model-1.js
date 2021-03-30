a028f2e48cbaaa805fcb4c9f4ce11085d8affeb9
Nina Brissey <nbrissey@gmail.com>
2021-03-29 23:08:34 -0700

    Add three number variables

--- model-1.js
+++ model-1.js
@@ -1,3 +1,7 @@
 var address = '614 Aspen St'
 var herAndHim = 'Jessica and Alex are perfect for one another.'
 var emojiFull = 'I\'m a fancy unicorn 🦄'
+
+var number = 10
+var age = 101
+var floatingInteger = 3.5
