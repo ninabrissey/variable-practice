e4e435e681c4d4dddc5871b3b004744673c31ce9
Nina Brissey <nbrissey@gmail.com>
2021-03-30 12:47:07 -0700

    Add one object and six variables

--- model-1.js
+++ model-1.js
@@ -13,3 +13,12 @@ var fastForward = true;
 var comediesTVWatched = ['Big Bang Theory', 'New Girl', 'Community'];
 var dramaskipped = ['long', 'mid-calf', 'short', 'bodycon'];
 var hikingDestinations = ['Muir Beach', 'Muir Woods', 'Towsley'];
+
+var tvShow = {
+  title = 'New Girl',
+  genre = 'Comedy'
+  type = 'series',
+  season = 2,
+  episode = 3,
+  runtime = 22.67
+};
