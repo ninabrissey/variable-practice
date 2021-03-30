c25989cd2d35146cdc7d759408ea7be663aa1dac
Nina Brissey <nbrissey@gmail.com>
2021-03-29 23:10:25 -0700

    Add semicolons to all variables

--- model-1.js
+++ model-1.js
@@ -1,7 +1,7 @@
-var address = '614 Aspen St'
-var herAndHim = 'Jessica and Alex are perfect for one another.'
-var emojiFull = 'I\'m a fancy unicorn 🦄'
+var address = '614 Aspen St';
+var herAndHim = 'Jessica and Alex are perfect for one another.';
+var emojiFull = 'I\'m a fancy unicorn 🦄';
 
-var number = 10
-var age = 101
-var floatingInteger = 3.5
+var number = 10;
+var age = 101;
+var floatingInteger = 3.5;
